d5ee3f9e1a8e262f16d034768bdbbd2fe87f2fd6
Pedro Goulart <pedro.goulart09@icloud.com>
2025-03-19 20:29:36 -0300

    actions creating

--- --myapp-ci/src/app/page.tsx
+++ --myapp-ci/src/app/page.tsx
@@ -1,8 +1,18 @@
+import './globals.css'
 
-export default function Home() {
+export default async function Home() {
+
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "force-cache",
+  });
+
+  const data = await res.json();
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1>Hello World</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
